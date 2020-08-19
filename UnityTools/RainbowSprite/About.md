6c5424d98d9cdc62714110cbad7cf04202364a84
CDog5 <36107588+CDog5@users.noreply.github.com>
2020-08-19 15:11:13 +0100

    Add files via upload

---
+++ UnityTools/RainbowSprite/About.md
@@ -0,0 +1,1 @@
+A script to make a sprite cycle through the colours of the rainbow!
---
+++ UnityTools/RainbowSprite/bg.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class bg : MonoBehaviour
+{
+public Gradient camgrad;
+	float value = 0f;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		StartCoroutine(bgrnd());
+
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+
+
+		if (value > 1)
+		{
+			value = 0;
+		}
+
+		//transform.Rotate(Vector3.forward, Time.deltaTime * 30f);
+	}
+	IEnumerator bgrnd()
+	{
+		while (true)
+		{
+			value += 0.05f;
+			GetComponent<Renderer>().material.color = camgrad.Evaluate(value);
+			yield return new WaitForSeconds(1);
+		}
+	}
+}
